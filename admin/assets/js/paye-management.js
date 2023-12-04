a280185ccf7aa303691950d3f5450e0513d49cc4
aliyukamilu <aliyukamilu002@gmail.com>
2023-12-04 06:22:51 +0100

    fetching special users done

---
+++ admin/assets/js/paye-management.js
@@ -0,0 +1,48 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get('type');
+
+async function fetchPayeUsers() {
+
+  const response = await fetch(`${HOST}/?getSpecialUsers`)
+  const specialUsers = await response.json()
+
+  $("#loader").css("display", "none")
+
+  if (specialUsers.status === 0) {
+    $('#dataTable').DataTable();
+
+  } else {
+
+    let theRightUSers = specialUsers.message.filter(rihuser => rihuser.category.toLowerCase() === category)
+
+    theRightUSers.forEach((rhUser, i) => {
+      let annual = rhUser.annual_estimate
+      let monthly = rhUser.monthly_estimate
+
+      let htmlData = ""
+
+      htmlData = `
+        <tr>
+          <td>${i + 1}</td>
+          <td>${rhUser.payer_id}</td>
+          <td>${rhUser.name}</td>
+          <td>${rhUser.category}</td>
+          <td>${rhUser.staff_quota}</td>
+          <td>&#8358; ${annual.toLocaleString("en-US")}</td>
+          <td>&#8358; ${monthly.toLocaleString("en-US")}</td>
+          <td>&#8358; 24,000,000</td>
+          <td><span class="badge bg-danger rounded-pill">Defaulter</span></td>
+          <td><a href="payedetails.html" class="btn btn-sm button-3">View</a></td>
+        </tr>
+      `
+
+      $("#payeMTabAll").append(htmlData)
+    });
+
+   
+
+  }
+
+}
+
+fetchPayeUsers()
