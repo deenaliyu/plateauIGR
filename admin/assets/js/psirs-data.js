e5e7b72cb9bbb2c60913449da116a528a7b03db2
aliyukamilu <aliyukamilu002@gmail.com>
2024-05-29 12:33:43 +0100

    edited the data

--- admin/assets/js/psirs-data.js
+++ admin/assets/js/psirs-data.js
@@ -1,294 +1,121 @@
 let usersName = [
   {
-    "name": "Abdullahi Musa",
-    "id": "PSIRS-1846",
+    "id": 390164,
+    "name": "JOSEPH IYANYA",
+    "tin": "23000043-7",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345678",
-    "lga": "Jos North",
-    "TIN": "12345678-4321"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Aisha Sadiq",
-    "id": "PSIRS-2479",
+    "id": 390165,
+    "name": "IORLAHA AHANGBA ISAAC",
+    "tin": "23205021-59",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345679",
-    "lga": "Jos South",
-    "TIN": "87654321-9876"
+    "phoneNumber": "7034372015",
+    "lga": "909",
+    "address": "UKADUM MAZA ROAD, JOS NORTH"
   },
   {
-    "name": "Amina Hassan",
-    "id": "PSIRS-3967",
+    "id": 390166,
+    "name": "ADANU SUNDAY SIMON",
+    "tin": "23200970-58",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345680",
-    "lga": "Barkin Ladi",
-    "TIN": "23456789-1234"
+    "phoneNumber": "8036197944",
+    "lga": "1239",
+    "address": "NURTW FANDA KARSHI"
   },
   {
-    "name": "Bello Abdullahi",
-    "id": "PSIRS-5123",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345681",
-    "lga": "Bassa",
-    "TIN": "98765432-5678"
-  },
-  {
-    "name": "Fatima Ibrahim",
-    "id": "PSIRS-6832",
+    "id": 390167,
+    "name": "DANIEL OKACHI",
+    "tin": "23000028-57",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345682",
-    "lga": "Bokkos",
-    "TIN": "34567890-2345"
+    "phoneNumber": "8164145770",
+    "lga": "954",
+    "address": "N/A"
   },
   {
-    "name": "Gambo Khadija",
-    "id": "PSIRS-7501",
+    "id": 390168,
+    "name": "LUCIE-ANN M LAHA",
+    "tin": "23000044-2",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345683",
-    "lga": "Jos East",
-    "TIN": "45678901-3456"
+    "phoneNumber": "8039668212",
+    "lga": "897",
+    "address": "SABON BARKI, JOS SOUTH"
   },
   {
-    "name": "Hassan Nafisa",
-    "id": "PSIRS-8425",
+    "id": 390169,
+    "name": "Mr. Emmauel O Agha",
+    "tin": "23000040-8",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345684",
-    "lga": "Kanam",
-    "TIN": "56789012-4567"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Hauwa Bello",
-    "id": "PSIRS-9742",
+    "id": 390171,
+    "name": "RAPHEAL ASIEGBU",
+    "tin": "23000040-9",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345685",
-    "lga": "Kanke",
-    "TIN": "67890123-5678"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Ibrahim Gambo",
-    "id": "PSIRS-1134",
+    "id": 390172,
+    "name": "daniel asabe mattah",
+    "tin": "23000041-2",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345686",
-    "lga": "Langtang North",
-    "TIN": "78901234-6789"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Khadija Zainab",
-    "id": "PSIRS-2267",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345687",
-    "lga": "Langtang South",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Musa Amina",
-    "id": "PSIRS-3198",
+    "id": 390173,
+    "name": "ZUMUNTAN MATA EKLISIYA COCIN LCC SARKIN MANGU",
+    "tin": "23205372-48",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345688",
-    "lga": "Mangu",
-    "TIN": "90123456-8901"
+    "phoneNumber": "8035701158",
+    "lga": "1431",
+    "address": "MANGU"
   },
   {
-    "name": "Nafisa Fatima",
-    "id": "PSIRS-4231",
+    "id": 390174,
+    "name": "JEROBOAM JEBULOL",
+    "tin": "23000040-2",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345689",
-    "lga": "Mikang",
-    "TIN": "01234567-9012"
+    "phoneNumber": "8105883615",
+    "lga": "1056",
+    "address": "N/A"
   },
   {
-    "name": "Sadiq Hauwa",
-    "id": "PSIRS-5719",
+    "id": 390175,
+    "name": "RABI'ATA YUSUF",
+    "tin": "23000041-8",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345690",
-    "lga": "Pankshin",
-    "TIN": "12345678-0123"
+    "phoneNumber": "7036832030",
+    "lga": "871",
+    "address": "RIKKOS, NEAR NDLEA OFFICE"
   },
   {
-    "name": "Zainab Abdullahi",
-    "id": "PSIRS-6138",
+    "id": 390176,
+    "name": "HYELDUGAL NIGERIA LIMITED",
+    "tin": "23000043-0",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345691",
-    "lga": "Qua'an Pan",
-    "TIN": "23456789-1234"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
-  {
-    "name": "Abdullahi Amina",
-    "id": "PSIRS-7824",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345692",
-    "lga": "Riyom",
-    "TIN": "34567890-2345"
-  },
-  {
-    "name": "Aisha Khadija",
-    "id": "PSIRS-8923",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345693",
-    "lga": "Shendam",
-    "TIN": "45678901-3456"
-  },
-  {
-    "name": "Amina Bello",
-    "id": "PSIRS-1037",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345694",
-    "lga": "Wase",
-    "TIN": "56789012-4567"
-  },
-  {
-    "name": "Bello Fatima",
-    "id": "PSIRS-2045",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345695",
-    "lga": "Jos North",
-    "TIN": "67890123-5678"
-  },
-  {
-    "name": "Fatima Hassan",
-    "id": "PSIRS-3578",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345696",
-    "lga": "Jos South",
-    "TIN": "78901234-6789"
-  },
-  {
-    "name": "Gambo Sadiq",
-    "id": "PSIRS-4729",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345697",
-    "lga": "Barkin Ladi",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Hassan Hauwa",
-    "id": "PSIRS-5347",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345698",
-    "lga": "Bassa",
-    "TIN": "90123456-8901"
-  },
-  {
-    "name": "Hauwa Ibrahim",
-    "id": "PSIRS-6890",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345699",
-    "lga": "Bokkos",
-    "TIN": "01234567-9012"
-  },
-  {
-    "name": "Ibrahim Nafisa",
-    "id": "PSIRS-7204",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345700",
-    "lga": "Jos East",
-    "TIN": "12345678-0123"
-  },
-  {
-    "name": "Khadija Musa",
-    "id": "PSIRS-8563",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345701",
-    "lga": "Kanam",
-    "TIN": "23456789-1234"
-  },
-  {
-    "name": "Musa Abdullahi",
-    "id": "PSIRS-9391",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345702",
-    "lga": "Kanke",
-    "TIN": "34567890-2345"
-  },
-  {
-    "name": "Nafisa Aisha",
-    "id": "PSIRS-1026",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345703",
-    "lga": "Langtang North",
-    "TIN": "45678901-3456"
-  },
-  {
-    "name": "Sadiq Amina",
-    "id": "PSIRS-2341",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345704",
-    "lga": "Langtang South",
-    "TIN": "56789012-4567"
-  },
-  {
-    "name": "Zainab Bello",
-    "id": "PSIRS-3087",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345705",
-    "lga": "Mangu",
-    "TIN": "67890123-5678"
-  },
-  {
-    "name": "Abdullahi Fatima",
-    "id": "PSIRS-4910",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345706",
-    "lga": "Mikang",
-    "TIN": "78901234-6789"
-  },
-  {
-    "name": "Aisha Gambo",
-    "id": "PSIRS-5648",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345707",
-    "lga": "Pankshin",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Amina Hassan",
-    "id": "PSIRS-6735",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345708",
-    "lga": "Qua'an Pan",
-    "TIN": "90123456-8901"
-  },
-  {
-    "name": "Bello Khadija",
-    "id": "PSIRS-7816",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345709",
-    "lga": "Riyom",
-    "TIN": "01234567-9012"
-  },
-  {
-    "name": "Fatima Musa",
-    "id": "PSIRS-8902",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345710",
-    "lga": "Shendam",
-    "TIN": "12345678-0123"
-  },
-  {
-    "name": "Gambo Nafisa",
-    "id": "PSIRS-1038",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345711",
-    "lga": "Wase",
-    "TIN": "23456789-1234"
-  }
 ]
 
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const shuffledUsers = shuffleArray(usersName);
-
-shuffledUsers.forEach((user, i) => {
+usersName.forEach((user, i) => {
   $("#showreport").append(`
     <tr>
       <td>${i + 1}</td>
-      <td>${user.TIN}</td>
+      <td>PSIRS-${user.id}</td>
+      <td>${user.tin}</td>
       <td>${user.name}</td>
       <td>
         ${user.accountStatus === "linked" ? '<span class="badge bg-success">linked</span>' : '<span class="badge bg-danger">un-linked</span>'}
--- admin/assets/js/psirs-details.js
+++ admin/assets/js/psirs-details.js
@@ -3,284 +3,120 @@ const userIdo = urlParams.get('id');
 
 let usersName = [
   {
-    "name": "Abdullahi Musa",
-    "id": "PSIRS-1846",
+    "id": 390164,
+    "name": "JOSEPH IYANYA",
+    "tin": "23000043-7",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345678",
-    "lga": "Jos North",
-    "TIN": "12345678-4321"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Aisha Sadiq",
-    "id": "PSIRS-2479",
+    "id": 390165,
+    "name": "IORLAHA AHANGBA ISAAC",
+    "tin": "23205021-59",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345679",
-    "lga": "Jos South",
-    "TIN": "87654321-9876"
+    "phoneNumber": "7034372015",
+    "lga": "909",
+    "address": "UKADUM MAZA ROAD, JOS NORTH"
   },
   {
-    "name": "Amina Hassan",
-    "id": "PSIRS-3967",
+    "id": 390166,
+    "name": "ADANU SUNDAY SIMON",
+    "tin": "23200970-58",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345680",
-    "lga": "Barkin Ladi",
-    "TIN": "23456789-1234"
+    "phoneNumber": "8036197944",
+    "lga": "1239",
+    "address": "NURTW FANDA KARSHI"
   },
   {
-    "name": "Bello Abdullahi",
-    "id": "PSIRS-5123",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345681",
-    "lga": "Bassa",
-    "TIN": "98765432-5678"
-  },
-  {
-    "name": "Fatima Ibrahim",
-    "id": "PSIRS-6832",
+    "id": 390167,
+    "name": "DANIEL OKACHI",
+    "tin": "23000028-57",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345682",
-    "lga": "Bokkos",
-    "TIN": "34567890-2345"
+    "phoneNumber": "8164145770",
+    "lga": "954",
+    "address": "N/A"
   },
   {
-    "name": "Gambo Khadija",
-    "id": "PSIRS-7501",
+    "id": 390168,
+    "name": "LUCIE-ANN M LAHA",
+    "tin": "23000044-2",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345683",
-    "lga": "Jos East",
-    "TIN": "45678901-3456"
+    "phoneNumber": "8039668212",
+    "lga": "897",
+    "address": "SABON BARKI, JOS SOUTH"
   },
   {
-    "name": "Hassan Nafisa",
-    "id": "PSIRS-8425",
+    "id": 390169,
+    "name": "Mr. Emmauel O Agha",
+    "tin": "23000040-8",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345684",
-    "lga": "Kanam",
-    "TIN": "56789012-4567"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Hauwa Bello",
-    "id": "PSIRS-9742",
+    "id": 390171,
+    "name": "RAPHEAL ASIEGBU",
+    "tin": "23000040-9",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345685",
-    "lga": "Kanke",
-    "TIN": "67890123-5678"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Ibrahim Gambo",
-    "id": "PSIRS-1134",
+    "id": 390172,
+    "name": "daniel asabe mattah",
+    "tin": "23000041-2",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345686",
-    "lga": "Langtang North",
-    "TIN": "78901234-6789"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
   {
-    "name": "Khadija Zainab",
-    "id": "PSIRS-2267",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345687",
-    "lga": "Langtang South",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Musa Amina",
-    "id": "PSIRS-3198",
+    "id": 390173,
+    "name": "ZUMUNTAN MATA EKLISIYA COCIN LCC SARKIN MANGU",
+    "tin": "23205372-48",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345688",
-    "lga": "Mangu",
-    "TIN": "90123456-8901"
+    "phoneNumber": "8035701158",
+    "lga": "1431",
+    "address": "MANGU"
   },
   {
-    "name": "Nafisa Fatima",
-    "id": "PSIRS-4231",
+    "id": 390174,
+    "name": "JEROBOAM JEBULOL",
+    "tin": "23000040-2",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345689",
-    "lga": "Mikang",
-    "TIN": "01234567-9012"
+    "phoneNumber": "8105883615",
+    "lga": "1056",
+    "address": "N/A"
   },
   {
-    "name": "Sadiq Hauwa",
-    "id": "PSIRS-5719",
+    "id": 390175,
+    "name": "RABI'ATA YUSUF",
+    "tin": "23000041-8",
     "accountStatus": "linked",
-    "phoneNumber": "+2347012345690",
-    "lga": "Pankshin",
-    "TIN": "12345678-0123"
+    "phoneNumber": "7036832030",
+    "lga": "871",
+    "address": "RIKKOS, NEAR NDLEA OFFICE"
   },
   {
-    "name": "Zainab Abdullahi",
-    "id": "PSIRS-6138",
+    "id": 390176,
+    "name": "HYELDUGAL NIGERIA LIMITED",
+    "tin": "23000043-0",
     "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345691",
-    "lga": "Qua'an Pan",
-    "TIN": "23456789-1234"
+    "phoneNumber": "N/A",
+    "lga": "N/A",
+    "address": "N/A"
   },
-  {
-    "name": "Abdullahi Amina",
-    "id": "PSIRS-7824",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345692",
-    "lga": "Riyom",
-    "TIN": "34567890-2345"
-  },
-  {
-    "name": "Aisha Khadija",
-    "id": "PSIRS-8923",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345693",
-    "lga": "Shendam",
-    "TIN": "45678901-3456"
-  },
-  {
-    "name": "Amina Bello",
-    "id": "PSIRS-1037",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345694",
-    "lga": "Wase",
-    "TIN": "56789012-4567"
-  },
-  {
-    "name": "Bello Fatima",
-    "id": "PSIRS-2045",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345695",
-    "lga": "Jos North",
-    "TIN": "67890123-5678"
-  },
-  {
-    "name": "Fatima Hassan",
-    "id": "PSIRS-3578",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345696",
-    "lga": "Jos South",
-    "TIN": "78901234-6789"
-  },
-  {
-    "name": "Gambo Sadiq",
-    "id": "PSIRS-4729",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345697",
-    "lga": "Barkin Ladi",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Hassan Hauwa",
-    "id": "PSIRS-5347",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345698",
-    "lga": "Bassa",
-    "TIN": "90123456-8901"
-  },
-  {
-    "name": "Hauwa Ibrahim",
-    "id": "PSIRS-6890",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345699",
-    "lga": "Bokkos",
-    "TIN": "01234567-9012"
-  },
-  {
-    "name": "Ibrahim Nafisa",
-    "id": "PSIRS-7204",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345700",
-    "lga": "Jos East",
-    "TIN": "12345678-0123"
-  },
-  {
-    "name": "Khadija Musa",
-    "id": "PSIRS-8563",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345701",
-    "lga": "Kanam",
-    "TIN": "23456789-1234"
-  },
-  {
-    "name": "Musa Abdullahi",
-    "id": "PSIRS-9391",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345702",
-    "lga": "Kanke",
-    "TIN": "34567890-2345"
-  },
-  {
-    "name": "Nafisa Aisha",
-    "id": "PSIRS-1026",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345703",
-    "lga": "Langtang North",
-    "TIN": "45678901-3456"
-  },
-  {
-    "name": "Sadiq Amina",
-    "id": "PSIRS-2341",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345704",
-    "lga": "Langtang South",
-    "TIN": "56789012-4567"
-  },
-  {
-    "name": "Zainab Bello",
-    "id": "PSIRS-3087",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345705",
-    "lga": "Mangu",
-    "TIN": "67890123-5678"
-  },
-  {
-    "name": "Abdullahi Fatima",
-    "id": "PSIRS-4910",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345706",
-    "lga": "Mikang",
-    "TIN": "78901234-6789"
-  },
-  {
-    "name": "Aisha Gambo",
-    "id": "PSIRS-5648",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345707",
-    "lga": "Pankshin",
-    "TIN": "89012345-7890"
-  },
-  {
-    "name": "Amina Hassan",
-    "id": "PSIRS-6735",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345708",
-    "lga": "Qua'an Pan",
-    "TIN": "90123456-8901"
-  },
-  {
-    "name": "Bello Khadija",
-    "id": "PSIRS-7816",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345709",
-    "lga": "Riyom",
-    "TIN": "01234567-9012"
-  },
-  {
-    "name": "Fatima Musa",
-    "id": "PSIRS-8902",
-    "accountStatus": "linked",
-    "phoneNumber": "+2347012345710",
-    "lga": "Shendam",
-    "TIN": "12345678-0123"
-  },
-  {
-    "name": "Gambo Nafisa",
-    "id": "PSIRS-1038",
-    "accountStatus": "unlinked",
-    "phoneNumber": "+2347012345711",
-    "lga": "Wase",
-    "TIN": "23456789-1234"
-  }
 ]
-
 
 const enumerated = urlParams.get('enumerated')
 let userrrData = {}
 
-let theUSerss = usersName.find((userIdolo => userIdolo.id === userIdo))
+let theUSerss = usersName.find((userIdolo => userIdolo.id === parseInt(userIdo)))
+
 let theimg = "./assets/img/avatars/1.png"
 
 $("#userInfo").html(`
@@ -288,16 +124,16 @@ $("#userInfo").html(`
     <img src="${theimg}" class="h-[70px] w-[70px] object-cover rounded-full" />
     <div class="mt-2">
     <h6 class="font-bold text-[20px]">${theUSerss.name}</h6>
-    <p><span class="font-bold">TIN:</span> ${theUSerss.TIN}</p>
+    <p><span class="font-bold">TIN:</span> ${theUSerss.tin}</p>
     </div>
     </div>
       
         <div class="flex flex-wrap gap-x-5 gap-y-3 mt-2">
-          <p><span class="font-bold">User ID:</span> ${theUSerss.id}</p>
+          <p><span class="font-bold">User ID:</span> PSIRS-${theUSerss.id}</p>
           <p><span class="font-bold">Category:</span> -</p>
           <p><span class="font-bold">State:</span> Plateau</p>
           <p><span class="font-bold">LGA:</span> ${theUSerss.lga}</p>
-          <p><span class="font-bold">Address:</span> -</p>
+          <p><span class="font-bold">Address:</span> ${theUSerss.address}</p>
           <p><span class="font-bold">Email address:</span> ${theUSerss.name.replaceAll(' ', '').toLowerCase() + '@gmail.com'}</p>
           <p><span class="font-bold">Contact:</span> ${theUSerss.phoneNumber}</p>
         </div>
