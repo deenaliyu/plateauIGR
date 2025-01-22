20667ade40a985d7d64e9b6d7fd83b83b9d932b1
aliyukamilu <aliyukamilu002@gmail.com>
2025-01-22 13:20:13 +0100

    analytics done

--- mda/assets/js/nav.js
+++ mda/assets/js/nav.js
@@ -67,6 +67,15 @@ if (payment === "yes") {
           <div data-i18n="Analytics"  class = "text-[2vh]">Dashboard</div>
         </a>
       </li>
+
+      <li class="menu-item">
+        <a href="analytics.html" class="menu-link anala">
+          <iconify-icon icon="majesticons:analytics-line" class = "menu-icon tf-icons"></iconify-icon>
+          <div data-i18n="Analytics"  class = "text-[2vh]">Analytics</div>
+        </a>
+      </li>
+
+
       <li class="menu-item">
         <a href="revenue.html" class="menu-link das">
           <iconify-icon icon="mdi:file-document-outline" class='menu-icon tf-icons'></iconify-icon>
