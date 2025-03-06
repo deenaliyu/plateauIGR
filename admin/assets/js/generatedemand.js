c0429f4d8ed718101d48ce2622c6fc4696441f3c
aliyukamilu <aliyukamilu002@gmail.com>
2025-03-06 07:20:28 +0100

    generate demand notice

--- admin/assets/js/generatedemand.js
+++ admin/assets/js/generatedemand.js
@@ -194,61 +194,72 @@ $("#sectorSelect").on("change", function () {
 })
 
 async function fetchRevHeads(sector) {
+  $("#revenueHeadItems").html(`
+    <div class="flex justify-center items-center mt-4">
+      <div class="animate-spin rounded-full h-10 w-10 border-b-2 border-gray-900"></div>
+    </div>
+  `)
+
   const response = await fetch(`${HOST}/?getAllRevenueHeads&demand_notice=yes&sector=${sector}`)
   const revHeads = await response.json()
 
   if (revHeads.status === 0) {
+    $("#revenueHeadItems").html("<p class='text-center'>No Items found for this Secotr</p>")
   } else {
 
     theRevs = revHeads.message
     // console.log(theRevs)
     $("#revenueHeadItems").html("")
     let revenueArr = []
-
+    let theItemNo = 0
     theRevs.forEach((dd, i) => {
       revenueArr.push(dd)
 
-      $("#revenueHeadItems").append(`
-        <div class="mb-2 bg-white p-4 rounded-lg shadow-md">
-          <h3 class="text-xl font-semibold text-gray-800 mb-4">Item - ${i+1}</h3>
-          <div class="flex items-center gap-2 mb-2">
-
-            <div class="form-group w-8/12">
-              <label for="">Assessment Informations*</label>
-              <select class="form-select genInv revHeadsss revenueHeader" required name="" >
-                <option value="${dd.id}" selected>${dd.COL_4} - (${dd.COL_5})</option>
-              </select>
+      if (dd.sector === sector) {
+        theItemNo++
+        $("#revenueHeadItems").append(`
+          <div class="mb-2 bg-white p-4 rounded-lg shadow-md">
+            <h3 class="text-xl font-semibold text-gray-800 mb-4">Item - ${theItemNo}</h3>
+            <div class="flex items-center gap-2 mb-2">
+  
+              <div class="form-group w-8/12">
+                <label for="">Assessment Informations*</label>
+                <select class="form-select genInv revHeadsss revenueHeader" required name="" >
+                  <option value="${dd.id}" selected>${dd.COL_4} - (${dd.COL_5})</option>
+                </select>
+              </div>
+  
+              <div class="form-group w-4/12">
+                <label for="">Amount*</label>
+                <input type="text" class="form-control genInv h-[40px] thePaymentInput amountTopay amountTopa"
+                  id="amountTopay">
+              </div>
+  
             </div>
-
-            <div class="form-group w-4/12">
-              <label for="">Amount*</label>
-              <input type="text" class="form-control genInv h-[40px] thePaymentInput amountTopay amountTopa"
-                id="amountTopay">
-            </div>
-
-          </div>
-
-          <div class="flex items-center gap-2">
-            <div class="form-group w-full">
-              <label for="">Previous Year *</label>
-              <input type="text" class="form-control form-control-sm genInv prevYears" id="previous_year"
-                placeholder="Year 1" required>
-
-              <input type="text" class="form-control mt-2 form-control-sm genInv prevYears2" id="previous_year2"
-                placeholder="Year 2" required>
-            </div>
-
-            <div class="form-group w-full">
-              <label for="">Previous Year Amount *</label>
-              <input type="text" class="form-control form-control-sm genInv prevAmounts" id="previous_year_value"
-                placeholder="Year 1 Amount" required>
-
-              <input type="text" class="form-control mt-2 form-control-sm genInv prevAmounts2"
-                id="previous_year_value2" placeholder="Year 2 Amount" required>
-            </div>
+  
+            <div class="flex items-center gap-2">
+              <div class="form-group w-full">
+                <label for="">Previous Year *</label>
+                <input type="text" class="form-control form-control-sm genInv prevYears" id="previous_year"
+                  placeholder="Year 1" required>
+  
+                <input type="text" class="form-control mt-2 form-control-sm genInv prevYears2" id="previous_year2"
+                  placeholder="Year 2" required>
+              </div>
+  
+              <div class="form-group w-full">
+                <label for="">Previous Year Amount *</label>
+                <input type="text" class="form-control form-control-sm genInv prevAmounts" id="previous_year_value"
+                  placeholder="Year 1 Amount" required>
+  
+                <input type="text" class="form-control mt-2 form-control-sm genInv prevAmounts2"
+                  id="previous_year_value2" placeholder="Year 2 Amount" required>
+              </div>
+            </div> 
           </div> 
-        </div> 
-      `)
+        `)
+      }
+
     })
 
     AllRevs = revenueArr
