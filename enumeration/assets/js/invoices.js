62819203124fa15287b95056213daed59e75e804
aliyukamilu <aliyukamilu002@gmail.com>
2025-05-16 23:21:12 +0100

    invoices

--- enumeration/assets/js/invoices.js
+++ enumeration/assets/js/invoices.js
@@ -12,129 +12,129 @@ $("#genInvBtn").on("click", function(){
     window.location.href = `../generateinvoice.html?created_by=enumerator&id=${userInfo2?.id}`
 })
 
-// async function fetchInvoice() {
+async function fetchInvoice() {
 
-//   $("#showThem").html("");
-//   $("#loader").css("display", "flex");
+  $("#showThem").html("");
+  $("#loader").css("display", "flex");
 
-//   let config = {
-//     mode: "cors",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Methods": "*",
-//     },
-//   };
-//   const response = await fetch(
-//     `${HOST}?AllInvoices`
-//   );
-//   const userInvoices = await response.json();
-//   // console.log(userInvoices);
-//   if (userInvoices.status === 0){
-//     let tt = 0;
-//     $("#totalInv").html(tt)
-//   }else{
-//     $("#totalInv").html(userInvoices.message.length)
-//   }
+  let config = {
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+    },
+  };
+  const response = await fetch(
+    `${HOST}?getAllInvoiceEnumerator&enum_id=${userInfo2?.id}`
+  );
+  const userInvoices = await response.json();
+  // console.log(userInvoices);
+  if (userInvoices.status === 0){
+    let tt = 0;
+    $("#totalInv").html(tt)
+  }else{
+    $("#totalInv").html(userInvoices.message.length)
+  }
  
-//   $("#loader").css("display", "none");
-//   if (userInvoices.status === 1) {
-//     AllInvoiceData =  userInvoices.message
+  $("#loader").css("display", "none");
+  if (userInvoices.status === 1) {
+    AllInvoiceData =  userInvoices.message
     
-//     displayData(userInvoices.message.reverse())
-//   } else {
-//     // $("#showInvoice").html("<tr></tr>");
-//     $("#dataTable").DataTable();
-//   }
-// }
+    displayData(userInvoices.message.reverse())
+  } else {
+    // $("#showInvoice").html("<tr></tr>");
+    $("#dataTable").DataTable();
+  }
+}
 
-// function displayData(userInvoices) {
-//     userInvoices.forEach((userInvoice, i) => {
-//       let addd = ""
-//       addd += `
-//         <tr class="relative">
-//         <td>${i + 1}</td>
-//         <td>${userInvoice.tax_number}</td>
-//         <td>${userInvoice.COL_3}</td>
-//         <td>${userInvoice.COL_4}</td>
-//         <td>${userInvoice.first_name} ${userInvoice.surname}</td>
-//         <td>${userInvoice.invoice_number}</td>
-//         <td>&#8358; ${parseFloat(userInvoice.amount_paid).toLocaleString()}</td>
-//         <td>${userInvoice.date_created.split(" ")[0]}</td>
-//         <td>${userInvoice.due_date}</td>
-//           `
-//       if (userInvoice.payment_status === "paid") {
-//         addd += `
-//             <td id="" class="checking">
-//               <p class='text-success'>${userInvoice.payment_status}</p>
-//             </td>
+function displayData(userInvoices) {
+    userInvoices.forEach((userInvoice, i) => {
+      let addd = ""
+      addd += `
+        <tr class="relative">
+        <td>${i + 1}</td>
+        <td>${userInvoice.tax_number}</td>
+        <td>${userInvoice.COL_3}</td>
+        <td>${userInvoice.COL_4}</td>
+        <td>${userInvoice.first_name} ${userInvoice.surname}</td>
+        <td>${userInvoice.invoice_number}</td>
+        <td>&#8358; ${parseFloat(userInvoice.amount_paid).toLocaleString()}</td>
+        <td>${userInvoice.date_created.split(" ")[0]}</td>
+        <td>${userInvoice.due_date}</td>
+          `
+      if (userInvoice.payment_status === "paid") {
+        addd += `
+            <td id="" class="checking">
+              <p class='text-success'>${userInvoice.payment_status}</p>
+            </td>
             
-//             `
-//       } else {
-//         addd += `
-//             <td id="" class="checking">
-//               <p class='text-danger'>${userInvoice.payment_status}</p>
-//             </td>
-//             `
-//       }
+            `
+      } else {
+        addd += `
+            <td id="" class="checking">
+              <p class='text-danger'>${userInvoice.payment_status}</p>
+            </td>
+            `
+      }
 
-//       addd += `
-//         <td>
-//           <a href="./viewinvoice.html?invnumber=${userInvoice.invoice_number}&load=true" target="_blank" class="btn btn-primary btn-sm viewUser" >View Invoice</a>
-//         </td>
-//         </tr>
-//         `
-//       $("#showThem").append(addd);
-//       $("#showThem2").append(`
-//         <tr>
-//             <td>${i + 1}</td>
-//             <td>${userInvoice.tax_number}</td>
-//             <td>${userInvoice.COL_3.replace(/,/g, '')}</td>
-//             <td>${userInvoice.COL_4}</td>
-//             <td>${userInvoice.first_name.replace(/,/g, '')} ${userInvoice.surname.replace(/,/g, '')}</td>
-//             <td>${userInvoice.invoice_number}</td>
-//             <td>&#8358; ${userInvoice.amount_paid}</td>
-//             <td>${userInvoice.date_created.split(" ")[0]}</td>
-//             <td>${userInvoice.due_date}</td>
-//             <td>${userInvoice.payment_status}</td>
-//         </tr>
-//       `)
-//     });
-// }
+      addd += `
+        <td>
+          <a href="./viewinvoice.html?invnumber=${userInvoice.invoice_number}&load=true" target="_blank" class="btn btn-primary btn-sm viewUser" >View Invoice</a>
+        </td>
+        </tr>
+        `
+      $("#showThem").append(addd);
+      $("#showThem2").append(`
+        <tr>
+            <td>${i + 1}</td>
+            <td>${userInvoice.tax_number}</td>
+            <td>${userInvoice.COL_3.replace(/,/g, '')}</td>
+            <td>${userInvoice.COL_4}</td>
+            <td>${userInvoice.first_name.replace(/,/g, '')} ${userInvoice.surname.replace(/,/g, '')}</td>
+            <td>${userInvoice.invoice_number}</td>
+            <td>&#8358; ${userInvoice.amount_paid}</td>
+            <td>${userInvoice.date_created.split(" ")[0]}</td>
+            <td>${userInvoice.due_date}</td>
+            <td>${userInvoice.payment_status}</td>
+        </tr>
+      `)
+    });
+}
 
-// fetchInvoice().then((uu) => {
-//   $("#dataTable").DataTable();
-// });
+fetchInvoice().then((uu) => {
+  $("#dataTable").DataTable();
+});
 
-// async function fetchAnalytics() {
+async function fetchAnalytics() {
 
-//   let config = {
-//     mode: "cors",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Methods": "*",
-//     },
-//   };
-//   try {
-//     const response = await fetch(
-//       `${HOST}/php/index.php?getDashboardAnalyticsAdmin`
-//     );
+  let config = {
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+    },
+  };
+  try {
+    const response = await fetch(
+      `${HOST}/php/index.php?dashboardAnalyticsEnumerator&enum_id=${userInfo2?.id}`
+    );
 
-//     const userAnalytics = await response.json();
-//     $("#due_amount").html(userAnalytics.due_amount)
-//     $("#due_invoices").html(userAnalytics.due_invoices)
-//     $("#total_amount_invoiced").html(userAnalytics.total_amount_invoiced.toLocaleString())
-//     $("#total_amountP").html(userAnalytics.total_amount_paid.toLocaleString())
+    const userAnalytics = await response.json();
+    $("#due_amount").html(userAnalytics.due_amount)
+    $("#due_invoices").html(userAnalytics.due_invoices)
+    $("#total_amount_invoiced").html(userAnalytics.total_amount_invoiced.toLocaleString())
+    $("#total_amountP").html(userAnalytics.total_amount_paid.toLocaleString())
 
-//     let total = (userAnalytics.total_amount_paid / userAnalytics.total_amount_invoiced) * 100
-//     $("#Compliance").html(total + "%")
-//     // console.log(userAnalytics)
-//   } catch (error) {
-//     console.log(error)
-//   }
+    let total = (userAnalytics.total_amount_paid / userAnalytics.total_amount_invoiced) * 100
+    $("#Compliance").html(total + "%")
+    // console.log(userAnalytics)
+  } catch (error) {
+    console.log(error)
+  }
 
 
-// }
+}
 
-// fetchAnalytics()
+fetchAnalytics()
