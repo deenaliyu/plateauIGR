9d58de7d40610ace884868ee3ec43af65f923779
aliyukamilu <aliyukamilu002@gmail.com>
2025-07-30 15:30:15 +0100

    tin distribution

--- hospital/assets/js/dashboard.js
+++ hospital/assets/js/dashboard.js
@@ -20,6 +20,71 @@ function convertToPercentage(number, total) {
   return percentage.toFixed(2); // Round the percentage to two decimal places
 }
 
+async function taxpayerDistributionbyTin() {
+  try {
+    const response = await fetch(`${HOST}/?enumeratorTinAnalysis`)
+    const taxpayerDistribution = await response.json()
+
+
+    // $("#totalTinYes").html(taxpayerDistribution.total_tin_yes);
+    // $("#totalTinNo").html(taxpayerDistribution.total_tin_no);
+
+    // Plot the pie chart
+    plotTinDistributionGraph(taxpayerDistribution);
+
+  } catch (error) {
+    console.log('Error fetching TIN analysis ', error)
+    // $("#totalTinYes").html("0");
+    // $("#totalTinNo").html("0");
+  }
+
+}
+
+taxpayerDistributionbyTin()
+
+function plotTinDistributionGraph(data) {
+  var chartDom = document.getElementById('tinDistributionTaxpayer');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Taxpayer Distribution by TIN',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: data.total_tin_yes, name: 'With TIN', itemStyle: { color: '#4caf50' } },
+          { value: data.total_tin_no, name: 'Without TIN', itemStyle: { color: '#f44336' } }
+        ]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+}
+
 function plotGraph(theDattaa) {
   var chartDom = document.getElementById('dashbordChart');
   var myChart = echarts.init(chartDom);
